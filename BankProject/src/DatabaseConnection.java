ed1566302abbc057d38b672213c9aead59010ae9
sofia.costa <costa.sofia.1408@gmail.com>
2023-09-12 09:11:18 -0300

    Bank Project Database connected

--- BankProject/src/DatabaseConnection.java
+++ BankProject/src/DatabaseConnection.java
@@ -2,9 +2,9 @@ import java.sql.*;
 public class DatabaseConnection
 {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/JavaBank?useSSL=false";
-    private static final String USER = "root";
-    private static final String PASSWORD = "durda1130";
+    private static final String URL = "jdbc:mysql://localhost:3306/JavaBank?allowPublicKeyRetrieval=true&useSSL=false";
+    private static final String USER = "bot22";
+    private static final String PASSWORD = "Access";
 
     public static Connection getConnection() throws SQLException
     {
